28463863e3ed25a344b271a95d391515a7cbea68
niumumin <460886008@qq.com>
2016-06-07 08:15:18 +0800

    学无止境

--- Application/Home/Controller/StudyController.class.php
+++ Application/Home/Controller/StudyController.class.php
@@ -4,14 +4,22 @@ use Think\Controller;
 class StudyController extends Controller {
     public function index(){
        	$category = M('category');
+        $blog=M('blog');
+        if(I('get.categroy_id')){
+        $where['categroy_id']=I('get.categroy_id');
+        }
+        $where['parent_categroy_id']=7;
     	$list =$category->where("parent_id = 7")->select();
     		foreach($list as $v){
     		$category_list[$v['id']] = $v;
     	}
-        //var_dump($category_list);die();
-    	$blog = M('blog')->where("parent_categroy_id = 7")->order("create_time desc")->select();
+    	$blog_log = $blog->order("create_time desc")->select();
+        $blog_is = $blog->order('read_num desc')->select();
+        $blog_log= $blog->where($where)->select();
+        $this->assign('log',$blog_log);
+        $this->assign('bl',$blog_is);
     	$this->assign('study',$category_list);
-    	$this->assign('blog',$blog);
+    	$this->assign('blog',$blog_log);
        	$this->display(); 
     }
     
